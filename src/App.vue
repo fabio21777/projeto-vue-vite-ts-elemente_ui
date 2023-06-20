70c1a4489d96ca2d903b44e49346e39909dfb9cf
Fabio <fabio@itexto.com.br>
2023-06-20 08:55:09 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+
+    <div>
+      <nav>
+        <RouterLink to="/"><el-button type="primary">home</el-button></RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
