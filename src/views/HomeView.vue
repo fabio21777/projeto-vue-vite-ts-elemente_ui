2f9d6c2847ef44fe30ff7c53bf0bd8385c3df589
Fabio <fabio@itexto.com.br>
2023-07-05 09:01:41 -0300

    utilizando form elemente ui

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,55 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <main>
-    <h1>hello world</h1>
-  </main>
+  <el-form
+    ref="ruleFormRef"
+    :model="ruleForm"
+    status-icon
+    :rules="rules"
+    label-width="120px"
+    class="demo-ruleForm"
+    label-position="top"
+  >
+    <el-form-item label="nome do restaurante" prop="restaurantName" >
+      <el-input v-model="ruleForm.restaurantName" />
+    </el-form-item>
+  </el-form>
 </template>
+
+<script lang="ts" setup>
+import { reactive, ref } from 'vue'
+import type { FormInstance, FormRules } from 'element-plus'
+
+const ruleFormRef = ref<FormInstance>()
+
+const ruleForm = reactive({
+  restaurantName: '',
+})
+
+const validaterestaurantName = (rule: any, value: string, callback: any) => {
+  if (value === '') {
+    callback(new Error('Por favor, digite o nome do restaurante'))
+  } else {
+    callback()
+  }
+}
+
+const rules: FormRules<typeof ruleForm> = reactive({
+  restaurantName: [{ validator: validaterestaurantName, trigger: 'blur' }],
+})
+
+const submitForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  formEl.validate((valid) => {
+    if (valid) {
+      console.log('submit!')
+    } else {
+      console.log('error submit!')
+      return false
+    }
+  })
+}
+
+const resetForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  formEl.resetFields()
+}
+</script>
