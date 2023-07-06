b4fb1e47f017e681acf853435a804ac27b25067b
Fabio <fabio@itexto.com.br>
2023-07-06 09:13:12 -0300

    06-07 trabalhando com formularios

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,45 +8,63 @@
     class="demo-ruleForm"
     label-position="top"
   >
-    <el-form-item label="nome do restaurante" prop="restaurantName" >
+    <el-form-item label="nome do restaurante" prop="restaurantName">
       <el-input v-model="ruleForm.restaurantName" />
     </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm(ruleFormRef)"> Salvar </el-button>
+    </el-form-item>
   </el-form>
+  <ul>
+    <li v-for="restaurante in restauranteList.value" :key="restaurante.id">
+      {{ restaurante }}
+    </li>
+  </ul>
 </template>
 
 <script lang="ts" setup>
-import { reactive, ref } from 'vue'
-import type { FormInstance, FormRules } from 'element-plus'
+import type { FormInstance, FormRules } from "element-plus";
+import { reactive, ref } from "vue";
 
-const ruleFormRef = ref<FormInstance>()
+//
+const restauranteList: Ref<any[]> = ref([]);
+
+
+
+
+//forulario de cadastro de restaurante
+const ruleFormRef = ref<FormInstance>();
 
 const ruleForm = reactive({
-  restaurantName: '',
-})
+  restaurantName: "",
+});
 
 const validaterestaurantName = (rule: any, value: string, callback: any) => {
-  if (value === '') {
-    callback(new Error('Por favor, digite o nome do restaurante'))
+  if (value === "") {
+    callback(new Error("Por favor, digite o nome do restaurante"));
   } else {
-    callback()
+    callback();
   }
-}
+};
 
 const rules: FormRules<typeof ruleForm> = reactive({
-  restaurantName: [{ validator: validaterestaurantName, trigger: 'blur' }],
-})
+  restaurantName: [{ validator: validaterestaurantName, trigger: "blur" }],
+});
 
 const submitForm = (formEl: FormInstance | undefined) => {
-  if (!formEl) return
+  if (!formEl) return;
   formEl.validate((valid) => {
     if (valid) {
-      console.log('submit!')
+      restauranteList.value.push(ruleForm.restaurantName);
+
+      resetForm(formEl);
+      return true;
     } else {
-      console.log('error submit!')
-      return false
+      console.log("error submit!");
+      return false;
     }
-  })
-}
+  });
+};
 
 const resetForm = (formEl: FormInstance | undefined) => {
   if (!formEl) return
