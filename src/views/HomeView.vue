839e0c97fbe8e8966cb9f2835e6c29b9e584773f
fabio21777 <fabiosouza21777@gmail.com>
2023-07-10 08:49:13 -0300

    formulario com element ui e validações

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,33 +1,50 @@
 <template>
-  <el-form
-    ref="ruleFormRef"
-    :model="ruleForm"
-    status-icon
-    :rules="rules"
-    label-width="120px"
-    class="demo-ruleForm"
-    label-position="top"
-  >
-    <el-form-item label="nome do restaurante" prop="restaurantName">
-      <el-input v-model="ruleForm.restaurantName" />
+  <el-form ref="ruleFormRef" :model="ruleForm" status-icon :rules="rules" label-width="120px" class="demo-ruleForm"
+    label-position="top">
+    <el-form-item label="nome do restaurante" prop="nome">
+      <el-input v-model="ruleForm.nome" />
     </el-form-item>
+    <el-form-item label="Endereço" prop="endereco">
+      <el-input v-model="ruleForm.endereco" />
+    </el-form-item>
+    <el-form-item label="Status" prop="status">
+      <el-select v-model="ruleForm.status" class="m-2" placeholder="Select" size="large">
+        <el-option v-for="item in listStatus" :key="item" :label="item" :value="item" />
+      </el-select>
+    </el-form-item>
+
+
     <el-form-item>
       <el-button type="primary" @click="submitForm(ruleFormRef)"> Salvar </el-button>
-    </el-form-item>
+    </el-form-item> 
   </el-form>
   <ul>
-    <li v-for="restaurante in restauranteList.value" :key="restaurante.id">
+    <li v-for="restaurante in restauranteList" :key="restaurante.id">
+      {{ restaurante.nome }}
       {{ restaurante }}
     </li>
   </ul>
 </template>
 
 <script lang="ts" setup>
+import { reactive, ref, type Ref } from "vue";
 import type { FormInstance, FormRules } from "element-plus";
-import { reactive, ref } from "vue";
+
 
 //
-const restauranteList: Ref<any[]> = ref([]);
+const restauranteList: Ref<Restaurante[]> = ref([]);
+
+interface Restaurante {
+  nome: string;
+  endereco: string;
+  status: StuatusRestaurante;
+  pratos: string[];
+}
+
+
+type StuatusRestaurante = "Quero voltar" | "recomendo" | "não recomendo" | "tentar novamente";
+
+const listStatus = ["Quero voltar", "recomendo", "não recomendo", "tentar novamente"];
 
 
 
@@ -35,9 +52,13 @@ const restauranteList: Ref<any[]> = ref([]);
 //forulario de cadastro de restaurante
 const ruleFormRef = ref<FormInstance>();
 
-const ruleForm = reactive({
-  restaurantName: "",
+const ruleForm = reactive<Restaurante>({
+  nome: '',
+  endereco: '',
+  status: 'Quero voltar',
+  pratos: [],
 });
+
 
 const validaterestaurantName = (rule: any, value: string, callback: any) => {
   if (value === "") {
@@ -47,15 +68,30 @@ const validaterestaurantName = (rule: any, value: string, callback: any) => {
   }
 };
 
+const validateEndereco = (rule: any, value: string, callback: any) => {
+  if (value === "") {
+    callback(new Error("Por favor, digite o endereço do restaurante"));
+  } else {
+    callback();
+  }
+};
+
 const rules: FormRules<typeof ruleForm> = reactive({
-  restaurantName: [{ validator: validaterestaurantName, trigger: "blur" }],
+  nome: [{ validator: validaterestaurantName, trigger: "blur" }],
+  endereco: [{ validator: validateEndereco, trigger: "blur" }],
+  
 });
 
 const submitForm = (formEl: FormInstance | undefined) => {
   if (!formEl) return;
-  formEl.validate((valid) => {
+  formEl.validate((valid: any) => {
     if (valid) {
-      restauranteList.value.push(ruleForm.restaurantName);
+      restauranteList.value.push({
+        nome: ruleForm.nome,
+        endereco: ruleForm.endereco,
+        status: ruleForm.status,
+        pratos: [],
+      });
 
       resetForm(formEl);
       return true;
